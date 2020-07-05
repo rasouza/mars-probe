e5bb6e3d6b140f333ccfe2c2dc23d4359e988edc
R. A. Souza <alves.wm@gmail.com>
2020-07-05 01:44:53 -0300

    one-liner for require

--- app.rb
+++ app.rb
@@ -1,6 +1,4 @@
-Dir.glob(File.join('./domain', '**', '*.rb'), &method(:require)) # include all files
-Dir.glob(File.join('./errors', '**', '*.rb'), &method(:require)) # include all files
-Dir.glob(File.join('./ports', '**', '*.rb'), &method(:require)) # include all files
+Dir.glob(File.join('./{domain,errors,ports}', '**', '*.rb'), &method(:require)) # include all files
 
 cli = CLI.new(ARGF)
 cli.run
